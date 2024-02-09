deaba5e3e08546762ac4ab3c173b85605c316442
lucastimho <lucas.t.ho.lh@gmail.com>
2024-02-09 11:06:29 -0600

    adds myplot function

---
+++ R/myplot.R
@@ -0,0 +1,11 @@
+#' A Quadratic function
+#'
+#' @param x a numeric vector
+#'
+#' @return a quadratic function
+#' @export
+#'
+#' @examples myplot
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
